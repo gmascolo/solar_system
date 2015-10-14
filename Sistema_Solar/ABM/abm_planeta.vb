797b30e9b422fd5e2d91be678bdd84c7f1872a2d
gmascolo <gaston.mascolo@gmail.com>
2015-10-14 00:08:36 -0300

    Revert "1"
    This reverts commit 1c6cd8a3f46df04acf21f584e0c7d868eef3a243.

--- Sistema_Solar/ABM/abm_planeta.vb
+++ Sistema_Solar/ABM/abm_planeta.vb
@@ -4,17 +4,4 @@
         Me.MdiParent = Inicio
         Me.WindowState = FormWindowState.Maximized
     End Sub
-
-    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        Dim objplaneta As New BE.Planeta
-
-        objplaneta.pNombre = txtNombre.Text
-        objplaneta.pDiametro = txtDiametro.Text
-        objplaneta.pTemperatura = txtTemp.Text
-        objplaneta.pOrden = txtOrden.Text
-        objplaneta.pClasificacion = cbClasifica.SelectedText
-
-
-
-    End Sub
 End Class

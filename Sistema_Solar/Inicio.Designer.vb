daf7d61bba13841a223c1a93eb2a5f5bc867a717
gmascolo <gaston.mascolo@gmail.com>
2015-10-14 02:34:22 -0300

    1

--- Sistema_Solar/Inicio.Designer.vb
+++ Sistema_Solar/Inicio.Designer.vb
@@ -23,14 +23,15 @@ Partial Class Inicio
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
-        Me.ABMToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
-        Me.OpcionesToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
-        Me.ParametrosDBToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
-        Me.AltaPlanetaToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
-        Me.EstrellaToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
-        Me.SateliteToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
         Me.DatosToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
         Me.PlanetasToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ABMToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.EstrellaToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.AltaPlanetaToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.SateliteToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.OpcionesToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ParametrosDBToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.InfoToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
         Me.MenuStrip1.SuspendLayout()
         Me.SuspendLayout()
         '
@@ -43,44 +44,6 @@ Partial Class Inicio
         Me.MenuStrip1.TabIndex = 1
         Me.MenuStrip1.Text = "MenuStrip1"
         '
-        'ABMToolStripMenuItem
-        '
-        Me.ABMToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.EstrellaToolStripMenuItem, Me.AltaPlanetaToolStripMenuItem, Me.SateliteToolStripMenuItem})
-        Me.ABMToolStripMenuItem.Name = "ABMToolStripMenuItem"
-        Me.ABMToolStripMenuItem.Size = New System.Drawing.Size(45, 20)
-        Me.ABMToolStripMenuItem.Text = "ABM"
-        '
-        'OpcionesToolStripMenuItem
-        '
-        Me.OpcionesToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ParametrosDBToolStripMenuItem})
-        Me.OpcionesToolStripMenuItem.Name = "OpcionesToolStripMenuItem"
-        Me.OpcionesToolStripMenuItem.Size = New System.Drawing.Size(69, 20)
-        Me.OpcionesToolStripMenuItem.Text = "Opciones"
-        '
-        'ParametrosDBToolStripMenuItem
-        '
-        Me.ParametrosDBToolStripMenuItem.Name = "ParametrosDBToolStripMenuItem"
-        Me.ParametrosDBToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
-        Me.ParametrosDBToolStripMenuItem.Text = "Parametros DB"
-        '
-        'AltaPlanetaToolStripMenuItem
-        '
-        Me.AltaPlanetaToolStripMenuItem.Name = "AltaPlanetaToolStripMenuItem"
-        Me.AltaPlanetaToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
-        Me.AltaPlanetaToolStripMenuItem.Text = "Planeta"
-        '
-        'EstrellaToolStripMenuItem
-        '
-        Me.EstrellaToolStripMenuItem.Name = "EstrellaToolStripMenuItem"
-        Me.EstrellaToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
-        Me.EstrellaToolStripMenuItem.Text = "Estrella"
-        '
-        'SateliteToolStripMenuItem
-        '
-        Me.SateliteToolStripMenuItem.Name = "SateliteToolStripMenuItem"
-        Me.SateliteToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
-        Me.SateliteToolStripMenuItem.Text = "Satelite"
-        '
         'DatosToolStripMenuItem
         '
         Me.DatosToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.PlanetasToolStripMenuItem})
@@ -91,10 +54,54 @@ Partial Class Inicio
         'PlanetasToolStripMenuItem
         '
         Me.PlanetasToolStripMenuItem.Name = "PlanetasToolStripMenuItem"
-        Me.PlanetasToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.PlanetasToolStripMenuItem.Size = New System.Drawing.Size(118, 22)
         Me.PlanetasToolStripMenuItem.Text = "Planetas"
         '
-        'Form1
+        'ABMToolStripMenuItem
+        '
+        Me.ABMToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.EstrellaToolStripMenuItem, Me.AltaPlanetaToolStripMenuItem, Me.SateliteToolStripMenuItem})
+        Me.ABMToolStripMenuItem.Name = "ABMToolStripMenuItem"
+        Me.ABMToolStripMenuItem.Size = New System.Drawing.Size(45, 20)
+        Me.ABMToolStripMenuItem.Text = "ABM"
+        '
+        'EstrellaToolStripMenuItem
+        '
+        Me.EstrellaToolStripMenuItem.Name = "EstrellaToolStripMenuItem"
+        Me.EstrellaToolStripMenuItem.Size = New System.Drawing.Size(113, 22)
+        Me.EstrellaToolStripMenuItem.Text = "Estrella"
+        '
+        'AltaPlanetaToolStripMenuItem
+        '
+        Me.AltaPlanetaToolStripMenuItem.Name = "AltaPlanetaToolStripMenuItem"
+        Me.AltaPlanetaToolStripMenuItem.Size = New System.Drawing.Size(113, 22)
+        Me.AltaPlanetaToolStripMenuItem.Text = "Planeta"
+        '
+        'SateliteToolStripMenuItem
+        '
+        Me.SateliteToolStripMenuItem.Name = "SateliteToolStripMenuItem"
+        Me.SateliteToolStripMenuItem.Size = New System.Drawing.Size(113, 22)
+        Me.SateliteToolStripMenuItem.Text = "Satelite"
+        '
+        'OpcionesToolStripMenuItem
+        '
+        Me.OpcionesToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ParametrosDBToolStripMenuItem, Me.InfoToolStripMenuItem})
+        Me.OpcionesToolStripMenuItem.Name = "OpcionesToolStripMenuItem"
+        Me.OpcionesToolStripMenuItem.Size = New System.Drawing.Size(69, 20)
+        Me.OpcionesToolStripMenuItem.Text = "Opciones"
+        '
+        'ParametrosDBToolStripMenuItem
+        '
+        Me.ParametrosDBToolStripMenuItem.Name = "ParametrosDBToolStripMenuItem"
+        Me.ParametrosDBToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.ParametrosDBToolStripMenuItem.Text = "Parametros DB"
+        '
+        'InfoToolStripMenuItem
+        '
+        Me.InfoToolStripMenuItem.Name = "InfoToolStripMenuItem"
+        Me.InfoToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.InfoToolStripMenuItem.Text = "Info"
+        '
+        'Inicio
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
@@ -102,7 +109,7 @@ Partial Class Inicio
         Me.Controls.Add(Me.MenuStrip1)
         Me.IsMdiContainer = True
         Me.MainMenuStrip = Me.MenuStrip1
-        Me.Name = "Form1"
+        Me.Name = "Inicio"
         Me.Text = "Form1"
         Me.MenuStrip1.ResumeLayout(False)
         Me.MenuStrip1.PerformLayout()
@@ -119,4 +126,5 @@ Partial Class Inicio
     Friend WithEvents ParametrosDBToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
     Friend WithEvents DatosToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
     Friend WithEvents PlanetasToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents InfoToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
 End Class

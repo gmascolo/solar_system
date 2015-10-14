1c6cd8a3f46df04acf21f584e0c7d868eef3a243
gmascolo <gaston.mascolo@gmail.com>
2015-10-14 00:02:21 -0300

    1

---
+++ Sistema_Solar/ABM/abm_planeta.Designer.vb
@@ -0,0 +1,403 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class abm_planeta
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.TabControl1 = New System.Windows.Forms.TabControl()
+        Me.Alta = New System.Windows.Forms.TabPage()
+        Me.Baja = New System.Windows.Forms.TabPage()
+        Me.Modificacion = New System.Windows.Forms.TabPage()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtNombre = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtDiametro = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.txtTemp = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.cbClasifica = New System.Windows.Forms.ComboBox()
+        Me.txtOrden = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.TextBox5 = New System.Windows.Forms.TextBox()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.TextBox6 = New System.Windows.Forms.TextBox()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.ComboBox2 = New System.Windows.Forms.ComboBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Label11 = New System.Windows.Forms.Label()
+        Me.ComboBox3 = New System.Windows.Forms.ComboBox()
+        Me.TabControl1.SuspendLayout()
+        Me.Alta.SuspendLayout()
+        Me.Baja.SuspendLayout()
+        Me.Modificacion.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'TabControl1
+        '
+        Me.TabControl1.Controls.Add(Me.Alta)
+        Me.TabControl1.Controls.Add(Me.Baja)
+        Me.TabControl1.Controls.Add(Me.Modificacion)
+        Me.TabControl1.Location = New System.Drawing.Point(13, 13)
+        Me.TabControl1.Name = "TabControl1"
+        Me.TabControl1.SelectedIndex = 0
+        Me.TabControl1.Size = New System.Drawing.Size(482, 282)
+        Me.TabControl1.TabIndex = 0
+        '
+        'Alta
+        '
+        Me.Alta.Controls.Add(Me.Button1)
+        Me.Alta.Controls.Add(Me.Label5)
+        Me.Alta.Controls.Add(Me.txtOrden)
+        Me.Alta.Controls.Add(Me.cbClasifica)
+        Me.Alta.Controls.Add(Me.Label4)
+        Me.Alta.Controls.Add(Me.txtTemp)
+        Me.Alta.Controls.Add(Me.Label3)
+        Me.Alta.Controls.Add(Me.txtDiametro)
+        Me.Alta.Controls.Add(Me.Label2)
+        Me.Alta.Controls.Add(Me.txtNombre)
+        Me.Alta.Controls.Add(Me.Label1)
+        Me.Alta.Location = New System.Drawing.Point(4, 22)
+        Me.Alta.Name = "Alta"
+        Me.Alta.Padding = New System.Windows.Forms.Padding(3)
+        Me.Alta.Size = New System.Drawing.Size(474, 256)
+        Me.Alta.TabIndex = 0
+        Me.Alta.Text = "Alta"
+        Me.Alta.UseVisualStyleBackColor = True
+        '
+        'Baja
+        '
+        Me.Baja.Controls.Add(Me.Button3)
+        Me.Baja.Controls.Add(Me.ComboBox2)
+        Me.Baja.Location = New System.Drawing.Point(4, 22)
+        Me.Baja.Name = "Baja"
+        Me.Baja.Padding = New System.Windows.Forms.Padding(3)
+        Me.Baja.Size = New System.Drawing.Size(474, 256)
+        Me.Baja.TabIndex = 1
+        Me.Baja.Text = "Baja"
+        Me.Baja.UseVisualStyleBackColor = True
+        '
+        'Modificacion
+        '
+        Me.Modificacion.Controls.Add(Me.ComboBox3)
+        Me.Modificacion.Controls.Add(Me.Label11)
+        Me.Modificacion.Controls.Add(Me.Button4)
+        Me.Modificacion.Controls.Add(Me.Label6)
+        Me.Modificacion.Controls.Add(Me.TextBox3)
+        Me.Modificacion.Controls.Add(Me.ComboBox1)
+        Me.Modificacion.Controls.Add(Me.Label7)
+        Me.Modificacion.Controls.Add(Me.TextBox4)
+        Me.Modificacion.Controls.Add(Me.Label8)
+        Me.Modificacion.Controls.Add(Me.TextBox5)
+        Me.Modificacion.Controls.Add(Me.Label9)
+        Me.Modificacion.Controls.Add(Me.TextBox6)
+        Me.Modificacion.Controls.Add(Me.Label10)
+        Me.Modificacion.Location = New System.Drawing.Point(4, 22)
+        Me.Modificacion.Name = "Modificacion"
+        Me.Modificacion.Padding = New System.Windows.Forms.Padding(3)
+        Me.Modificacion.Size = New System.Drawing.Size(474, 256)
+        Me.Modificacion.TabIndex = 2
+        Me.Modificacion.Text = "Modificacion"
+        Me.Modificacion.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(20, 80)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(44, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Nombre"
+        '
+        'txtNombre
+        '
+        Me.txtNombre.Location = New System.Drawing.Point(21, 96)
+        Me.txtNombre.Name = "txtNombre"
+        Me.txtNombre.Size = New System.Drawing.Size(100, 20)
+        Me.txtNombre.TabIndex = 1
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(176, 80)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(49, 13)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "Diametro"
+        '
+        'txtDiametro
+        '
+        Me.txtDiametro.Location = New System.Drawing.Point(179, 96)
+        Me.txtDiametro.Name = "txtDiametro"
+        Me.txtDiametro.Size = New System.Drawing.Size(100, 20)
+        Me.txtDiametro.TabIndex = 3
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(20, 141)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(67, 13)
+        Me.Label3.TabIndex = 4
+        Me.Label3.Text = "Temperatura"
+        '
+        'txtTemp
+        '
+        Me.txtTemp.Location = New System.Drawing.Point(21, 157)
+        Me.txtTemp.Name = "txtTemp"
+        Me.txtTemp.Size = New System.Drawing.Size(100, 20)
+        Me.txtTemp.TabIndex = 5
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(337, 80)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(66, 13)
+        Me.Label4.TabIndex = 6
+        Me.Label4.Text = "Clasificación"
+        '
+        'cbClasifica
+        '
+        Me.cbClasifica.FormattingEnabled = True
+        Me.cbClasifica.Items.AddRange(New Object() {"Exterior", "Interior"})
+        Me.cbClasifica.Location = New System.Drawing.Point(340, 96)
+        Me.cbClasifica.Name = "cbClasifica"
+        Me.cbClasifica.Size = New System.Drawing.Size(114, 21)
+        Me.cbClasifica.TabIndex = 7
+        '
+        'txtOrden
+        '
+        Me.txtOrden.Location = New System.Drawing.Point(179, 157)
+        Me.txtOrden.Name = "txtOrden"
+        Me.txtOrden.Size = New System.Drawing.Size(100, 20)
+        Me.txtOrden.TabIndex = 8
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(179, 140)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(36, 13)
+        Me.Label5.TabIndex = 9
+        Me.Label5.Text = "Orden"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(179, 140)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(36, 13)
+        Me.Label6.TabIndex = 19
+        Me.Label6.Text = "Orden"
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(179, 157)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox3.TabIndex = 18
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Items.AddRange(New Object() {"Exterior", "Interior"})
+        Me.ComboBox1.Location = New System.Drawing.Point(340, 96)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(114, 21)
+        Me.ComboBox1.TabIndex = 17
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(337, 80)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(66, 13)
+        Me.Label7.TabIndex = 16
+        Me.Label7.Text = "Clasificación"
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Location = New System.Drawing.Point(21, 157)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox4.TabIndex = 15
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(20, 141)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(67, 13)
+        Me.Label8.TabIndex = 14
+        Me.Label8.Text = "Temperatura"
+        '
+        'TextBox5
+        '
+        Me.TextBox5.Location = New System.Drawing.Point(179, 96)
+        Me.TextBox5.Name = "TextBox5"
+        Me.TextBox5.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox5.TabIndex = 13
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Location = New System.Drawing.Point(176, 80)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(49, 13)
+        Me.Label9.TabIndex = 12
+        Me.Label9.Text = "Diametro"
+        '
+        'TextBox6
+        '
+        Me.TextBox6.Location = New System.Drawing.Point(21, 96)
+        Me.TextBox6.Name = "TextBox6"
+        Me.TextBox6.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox6.TabIndex = 11
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Location = New System.Drawing.Point(20, 80)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(44, 13)
+        Me.Label10.TabIndex = 10
+        Me.Label10.Text = "Nombre"
+        '
+        'ComboBox2
+        '
+        Me.ComboBox2.FormattingEnabled = True
+        Me.ComboBox2.Location = New System.Drawing.Point(7, 25)
+        Me.ComboBox2.Name = "ComboBox2"
+        Me.ComboBox2.Size = New System.Drawing.Size(150, 21)
+        Me.ComboBox2.TabIndex = 0
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(200, 227)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 10
+        Me.Button1.Text = "Alta"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(416, 318)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 11
+        Me.Button2.Text = "Cerrar"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(190, 190)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 23)
+        Me.Button3.TabIndex = 1
+        Me.Button3.Text = "Button3"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(200, 227)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(75, 23)
+        Me.Button4.TabIndex = 20
+        Me.Button4.Text = "Modificar"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Label11
+        '
+        Me.Label11.AutoSize = True
+        Me.Label11.Location = New System.Drawing.Point(21, 24)
+        Me.Label11.Name = "Label11"
+        Me.Label11.Size = New System.Drawing.Size(104, 13)
+        Me.Label11.TabIndex = 21
+        Me.Label11.Text = "Seleccionar planeta:"
+        '
+        'ComboBox3
+        '
+        Me.ComboBox3.FormattingEnabled = True
+        Me.ComboBox3.Location = New System.Drawing.Point(154, 21)
+        Me.ComboBox3.Name = "ComboBox3"
+        Me.ComboBox3.Size = New System.Drawing.Size(121, 21)
+        Me.ComboBox3.TabIndex = 22
+        '
+        'abm_planeta
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(551, 353)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.TabControl1)
+        Me.Name = "abm_planeta"
+        Me.Text = "abm_planeta"
+        Me.TabControl1.ResumeLayout(False)
+        Me.Alta.ResumeLayout(False)
+        Me.Alta.PerformLayout()
+        Me.Baja.ResumeLayout(False)
+        Me.Modificacion.ResumeLayout(False)
+        Me.Modificacion.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents TabControl1 As System.Windows.Forms.TabControl
+    Friend WithEvents Alta As System.Windows.Forms.TabPage
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents txtOrden As System.Windows.Forms.TextBox
+    Friend WithEvents cbClasifica As System.Windows.Forms.ComboBox
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents txtTemp As System.Windows.Forms.TextBox
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents txtDiametro As System.Windows.Forms.TextBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents txtNombre As System.Windows.Forms.TextBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Baja As System.Windows.Forms.TabPage
+    Friend WithEvents Modificacion As System.Windows.Forms.TabPage
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents TextBox3 As System.Windows.Forms.TextBox
+    Friend WithEvents ComboBox1 As System.Windows.Forms.ComboBox
+    Friend WithEvents Label7 As System.Windows.Forms.Label
+    Friend WithEvents TextBox4 As System.Windows.Forms.TextBox
+    Friend WithEvents Label8 As System.Windows.Forms.Label
+    Friend WithEvents TextBox5 As System.Windows.Forms.TextBox
+    Friend WithEvents Label9 As System.Windows.Forms.Label
+    Friend WithEvents TextBox6 As System.Windows.Forms.TextBox
+    Friend WithEvents Label10 As System.Windows.Forms.Label
+    Friend WithEvents ComboBox2 As System.Windows.Forms.ComboBox
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents ComboBox3 As System.Windows.Forms.ComboBox
+    Friend WithEvents Label11 As System.Windows.Forms.Label
+    Friend WithEvents Button4 As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+End Class
